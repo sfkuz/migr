b59275520c4a5db9c7b26a376666c2f974962b5b
Sofiia Kuzmenko <kuzmenkosofi@gmail.com>
2025-11-21 21:30:28 +0100

    add validation

---
+++ src/lib/schemas/user.py
@@ -0,0 +1,25 @@
+# валидация
+
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from datetime import datetime
+
+class UserCreate(BaseModel): # создание, данные идут с cli
+    name: str
+    email: EmailStr
+    age: Optional[int] = None
+
+class UserUpdate(BaseModel): # обновление
+    name: Optional[str] = None
+    email: Optional[EmailStr] = None
+    age: Optional[int] = None
+    is_active: Optional[bool] = None
+
+class User(BaseModel): # main model
+    id: int
+    name: str
+    email: EmailStr
+    age: Optional[int]
+    is_active: bool
+    created_at: datetime
+    updated_at: datetime
