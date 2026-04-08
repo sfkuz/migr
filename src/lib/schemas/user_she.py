46cb362edc5beb242bd506b159eb3c179f4ad448
Sofiia Kuzmenko <kuzmenkosofi@gmail.com>
2026-04-08 19:47:16 +0200

    delete coments

--- src/lib/schemas/user_she.py
+++ src/lib/schemas/user_she.py
@@ -2,18 +2,18 @@ from pydantic import BaseModel, EmailStr
 from typing import Optional
 from datetime import datetime
 
-class UserCreate(BaseModel): # создание, данные идут с cli
+class UserCreate(BaseModel):
     name: str
     email: EmailStr
     age: Optional[int] = None
 
-class UserUpdate(BaseModel): # обновление
+class UserUpdate(BaseModel):
     name: Optional[str] = None
     email: Optional[EmailStr] = None
     age: Optional[int] = None
     is_active: Optional[bool] = None
 
-class User(BaseModel): # main model
+class User(BaseModel):
     id: int
     name: str
     email: EmailStr
