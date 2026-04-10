8f552792f4523e5a62933c799e934954cfc6dbaa
Sofiia Kuzmenko <kuzmenkosofi@gmail.com>
2026-04-10 13:32:08 +0200

    create domain repository for event

---
+++ src/domain/events/repositoris.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+import datetime
+from abc import ABC, abstractmethod
+from tkinter import EventType
+from typing import Iterable, Sequence
+from uuid import UUID
+
+from domain.events.entities import Event
+
+class IEventRepository(ABC):
+
+    @abstractmethod
+    async def add(self, event: Event) -> None:
+        ...
+
+    @abstractmethod
+    async def get_by_id(self, event_id: UUID) -> Event | None:
+        ...
+
+    @abstractmethod
+    async def get_all(self) -> Iterable[Event]:
+        ...
+
+    @abstractmethod
+    async def delete(self, event_id: UUID) -> None:
+        ...
+
+    @abstractmethod
+    async def find_by_event_type(self, event_type: EventType) -> Sequence[Event]:
+        ...
+
+    @abstractmethod
+    async def get_by_start_at(self, start_at: datetime) -> Sequence[Event]:
+        ...
